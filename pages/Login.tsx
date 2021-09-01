c7a719afad212dfda7603b0161f52db8700f00f4
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-09-01 07:42:28 -0300

    Login and Signup page redirect to main page if user is already logged

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import { LoginAndSingupHeader as Header } from '../components/Headers/LoginAndSignupHeader'
 import { LoginForm } from '../components/Forms/LoginForm'
+import { useSelector } from 'react-redux'
+import { useRouter } from 'next/dist/client/router'
 
 const Login: React.FC = () => {
+
+  const router = useRouter()
+  const loggedUser = useSelector(state => state.auth)
+
+  if (loggedUser) {
+    router.push('/')
+    return null
+  }
+
   return (
     <>
       <Header link={['/SignUp', 'Cadastrar']} />
--- pages/SignUp.tsx
+++ pages/SignUp.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import { LoginAndSingupHeader as Header } from '../components/Headers/LoginAndSignupHeader'
 import { SignUpForm } from '../components/Forms/SignupForm'
+import { useSelector } from 'react-redux'
+import { useRouter } from 'next/dist/client/router'
 
 const SignUp: React.FC = () => {
+
+  const router = useRouter()
+  const loggedUser = useSelector(state => state.auth)
+
+  if (loggedUser) {
+    router.push('/')
+    return null
+  }
+
   return (
     <>
       <Header link={['/Login', 'Entrar']} />
