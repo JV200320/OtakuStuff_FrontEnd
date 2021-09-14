18e8a203adcb13469442cde508f1321d4bd237f1
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-09-14 12:47:49 -0300

    Follow button loading state

--- components/Main/ContentPage/AnimePage/FollowAndUnfollowButton/UnfollowButton/index.tsx
+++ components/Main/ContentPage/AnimePage/FollowAndUnfollowButton/UnfollowButton/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Row, Button } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Row, Button, Spinner } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FavoriteService from '../../../../../../services/favorites'
 import { RootState } from '../../../../../../store/modules/rootReducer'
@@ -9,13 +9,16 @@ export const UnfollowButton = () => {
 
   const { id } = useSelector((state: RootState) => state.auth)
   const { id: anime_id } = useSelector((state: RootState) => state.contentPageToDisplay)
+  const [buttonLoading, setButtonLoading] = useState<boolean>(false)
 
   const dispatch = useDispatch()
 
   const handleRemoveFromFavorite = async () => {
+    setButtonLoading(true)
     let res = await FavoriteService.removeFromFavorite(id, anime_id)
     res.data.favorites = parseStringsInArrayToJSON(res.data.favorites)
     dispatch(updateFavorites(res.data.favorites))
+    setButtonLoading(false)
   }
 
   const parseStringsInArrayToJSON = (favoritesStringArray) => {
@@ -27,7 +30,13 @@ export const UnfollowButton = () => {
   return (
     <Row className="d-flex justify-content-center py-2">
       <Button className="w-50" variant='info' onClick={() => handleRemoveFromFavorite()}>
-        Parar de seguir
+        {
+          buttonLoading
+            ?
+            <Spinner animation="border" variant="light" />
+            :
+            'Parar de seguir'
+        }
       </Button>
     </Row>
   )
