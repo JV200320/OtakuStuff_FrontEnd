19328ae41459c8fbf97366ac1a1d678db6c676f2
Joao Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-11-12 22:57:42 -0300

    Creating cookie dto and AuthService's validateToken method

---
+++ dtos/Cookies.ts
@@ -0,0 +1,7 @@
+export interface Cookie {
+  "access-token": string,
+  uid: string,
+  client: string,
+  "token-type": string,
+  expiry: string
+}
--- services/auth/index.ts
+++ services/auth/index.ts
@@ -1,9 +1,11 @@
+import { Cookie } from "../../dtos/Cookies";
 import User from "../../dtos/User";
 import Api from "../api";
 import { parseFavoritesToJSON } from "../helpers/parseFavorites";
 
 interface Response {
   data: User
+  success?: boolean
 }
 
 const AuthService = {
@@ -16,6 +18,13 @@ const AuthService = {
   login: async (params) => {
     let res = await Api.post<Response>('/auth/sign_in', params)
     let user: User = res.data.data
+    user.favorites = parseFavoritesToJSON(user.favorites)
+    return user
+  },
+  validateToken: async (cookie: Cookie):Promise<User> => {
+    Api.defaults.headers = cookie
+    let res = await Api.get<Response>('auth/validate_token')
+    let user = res.data.data
     user.favorites = parseFavoritesToJSON(user.favorites)
     return user
   }
